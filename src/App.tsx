c402d4956b62fe8f799889efa138c6b0d2b6e19a
Malav Naagar <73930050+malav4all@users.noreply.github.com>
2023-05-24 14:07:26 +0530

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [seats, setSeats] = useState([]);
+  const [bookingCount, setBookingCount] = useState<any>();
+
+  useEffect(() => {
+    fetchSeats();
+  }, []);
+
+  const fetchSeats = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/api/seats");
+      setSeats(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const bookSeats = async () => {
+    try {
+      const response: any = await axios.post("http://localhost:8000/api/book", {
+        bookingCount,
+      });
+      if (response.status === 200) {
+        setSeats(response?.data?.updatedSeats);
+        alert("Seat booked SuccessFully");
+        setBookingCount("");
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 400) {
+        alert(error?.response?.data?.errorMessage);
+      }
+    }
+  };
+
+  const handleChange = (e: any) => {
+    const inputValue = e.target.value;
+    if (inputValue >= 1 && inputValue <= 7) {
+      setBookingCount(e.target.value);
+    }
+  };
+  const renderSeats = () => {
+    return seats.map((item: any, index: any) => {
+      return (
+        <>
+          <div
+            style={{
+              boxSizing: "border-box",
+              width: "10%",
+              padding: "20px",
+              border: "1px solid #000",
+              backgroundColor: item.isAvailable ? "red" : "green",
+            }}
+          >
+            <h1>
+              Seat No:{item.seatNumber}{" "}
+              {item.isAvailable ? "Booked" : "Available"}
+            </h1>
+          </div>
+        </>
+      );
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Train Seat Booking</h2>
+      <div>
+        <input
+          type="number"
+          min="1"
+          max="7"
+          value={bookingCount}
+          onChange={handleChange}
+        />
+        <button onClick={bookSeats}>Book Seats</button>
+        {/* Add more buttons for different seat counts */}
+      </div>
+      <div
+        className="seats-container"
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "70px",
+        }}
+      >
+        {renderSeats()}
+      </div>
+
+      {/* {bookingCount > 0 && <p>{bookingCount} seat(s) booked successfully!</p>} */}
     </div>
   );
 }
